89963be03731cc8d8efda40a91fab3d1e3065872
ijavacs <ijavacs@gmail.com>
2020-10-29 17:42:35 +0800

    倒计时 优化 ， 倒计时完成后 才可滑动

--- app/src/javascripts/main.js
+++ app/src/javascripts/main.js
@@ -1,14 +1,14 @@
+// load dependencies
+var animationControl = require('./animation-control.js');
+var bgMusic = $('audio').get(0);
+var $btnMusic = $('.btn-music');
+var $upArrow = $('.up-arrow');
+
 (function () {
     'use strict';
 
-    // load dependencies
-    var animationControl = require('./animation-control.js');
-
-
     $(document).ready(function () {
-        var bgMusic = $('audio').get(0);
-        var $btnMusic = $('.btn-music');
-        var $upArrow = $('.up-arrow');
+
 
         // background music control
         $btnMusic.click(function () {
@@ -21,49 +21,6 @@
             }
         });
 
-        // init Swiper
-        new Swiper('.swiper-container', {
-            mousewheelControl: true,
-            effect: 'fade',    // slide, fade, coverflow or flip
-            fadeEffect: {
-              crossFade: true
-            },
-            speed: 1000,
-            direction: 'vertical',
-            fade: {
-                crossFade: false
-            },
-            coverflow: {
-                rotate: 100,
-                stretch: 0,
-                depth: 300,
-                modifier: 1,
-                slideShadows: false     // do disable shadows for better performance
-            },
-            flip: {
-                limitRotation: true,
-                slideShadows: false     // do disable shadows for better performance
-            },
-            onInit: function (swiper) {
-                animationControl.initAnimationItems();  // get items ready for animations
-                animationControl.playAnimation(swiper); // play animations of the first slide
-            },
-            onTransitionStart: function (swiper) {     // on the last slide, hide .btn-swipe
-                if (swiper.activeIndex === swiper.slides.length - 1) {
-                    $upArrow.hide();
-                } else {
-                    $upArrow.show();
-                }
-            },
-            onTransitionEnd: function (swiper) {       // play animations of the current slide
-                animationControl.playAnimation(swiper);
-            },
-            onTouchStart: function (swiper, event) {    // mobile devices don't allow audios to play automatically, it has to be triggered by a user event(click / touch).
-                if (!$btnMusic.hasClass('paused') && bgMusic.paused) {
-                    bgMusic.play();
-                }
-            }
-        });
 
         // hide loading animation since everything is ready
         $('.loading-overlay').slideUp();
@@ -83,22 +40,22 @@ Vue.component('countdown', {
     <div v-show="!expired" class="timer">
       <div class="box">
         <div class="spacer"></div>
-        <p class="value">{{ theTime.days }}</p>
+        <p class="value">{{ days }}</p>
         <p class="label">days</p>
       </div>
       <div class="box">
         <div class="spacer"></div>
-        <p class="value">{{ theTime.hours }}</p>
+        <p class="value">{{ hours }}</p>
         <p class="label">hours</p>
       </div>
       <div class="box">
         <div class="spacer"></div>
-        <p class="value">{{ theTime.minutes }}</p>
+        <p class="value">{{ minutes }}</p>
         <p class="label">minutes</p>
       </div>
       <div class="box">
         <div class="spacer"></div>
-        <p class="value">{{ theTime.seconds }}</p>
+        <p class="value">{{ seconds }}</p>
         <p class="label">seconds</p>
       </div>
       <p class="text">to meet you</p>
@@ -126,47 +83,80 @@ Vue.component('countdown', {
             expired: false
         };
     },
+    created() {
+        var ctx = this,
+            countDownDate = new Date(ctx.deadline).getTime();
+        let firstInit = true;
 
-    computed: {
-        theTime() {
-            var ctx = this,
-                countDownDate = new Date(ctx.deadline).getTime();
+        // Countdown loop
+        const clear = setInterval(function() {
 
-            // Countdown loop
-            var x = setInterval(function() {
+            // Difference between the 2 dates
+            var countDownDate = new Date(ctx.deadline).getTime(),
+                now = new Date().getTime(),
+                diff = countDownDate - now,
 
-                // Difference between the 2 dates
-                var countDownDate = new Date(ctx.deadline).getTime(),
-                    now = new Date().getTime(),
-                    diff = countDownDate - now,
+                // Time conversion to days, hours, minutes and seconds
+                tdays = Math.floor(diff / (1000 * 60 * 60 * 24)),
+                thours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+                tminutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)),
+                tseconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-                    // Time conversion to days, hours, minutes and seconds
-                    tdays = Math.floor(diff / (1000 * 60 * 60 * 24)),
-                    thours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-                    tminutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)),
-                    tseconds = Math.floor((diff % (1000 * 60)) / 1000);
+            // Keep 2 digits
+            ctx.days = (tdays < 10) ? '0' + tdays : tdays;
+            ctx.hours = (thours < 10) ? '0' + thours : thours;
+            ctx.minutes = (tminutes < 10) ? '0' + tminutes : tminutes;
+            ctx.seconds = (tseconds < 10) ? '0' + tseconds : tseconds;
 
-                // Keep 2 digits
-                ctx.days = (tdays < 10) ? '0' + tdays : tdays;
-                ctx.hours = (thours < 10) ? '0' + thours : thours;
-                ctx.minutes = (tminutes < 10) ? '0' + tminutes : tminutes;
-                ctx.seconds = (tseconds < 10) ? '0' + tseconds : tseconds;
-
-                // Check for time expiration
-                if (diff < 0) {
-                    clearInterval(x);
-                    ctx.expired = true;
-                }
-            }, 1000);
-
-            // Return data
-            return {
-                days: ctx.days,
-                hours: ctx.hours,
-                minutes: ctx.minutes,
-                seconds: ctx.seconds
-            };
-        }
+            // Check for time expiration
+            if (diff < 0) {
+                clearInterval(clear);
+                ctx.expired = true;
+                // init Swiper
+                new Swiper('.swiper-container', {
+                    mousewheelControl: true,
+                    effect: 'fade',    // slide, fade, coverflow or flip
+                    fadeEffect: {
+                        crossFade: true
+                    },
+                    speed: 600,
+                    direction: 'vertical',
+                    fade: {
+                        crossFade: false
+                    },
+                    coverflow: {
+                        rotate: 100,
+                        stretch: 0,
+                        depth: 300,
+                        modifier: 1,
+                        slideShadows: false     // do disable shadows for better performance
+                    },
+                    flip: {
+                        limitRotation: true,
+                        slideShadows: false     // do disable shadows for better performance
+                    },
+                    onInit: function (swiper) {
+                        animationControl.initAnimationItems();  // get items ready for animations
+                        animationControl.playAnimation(swiper); // play animations of the first slide
+                    },
+                    onTransitionStart: function (swiper) {     // on the last slide, hide .btn-swipe
+                        if (swiper.activeIndex === swiper.slides.length - 1) {
+                            $upArrow.hide();
+                        } else {
+                            $upArrow.show();
+                        }
+                    },
+                    onTransitionEnd: function (swiper) {       // play animations of the current slide
+                        animationControl.playAnimation(swiper);
+                    },
+                    onTouchStart: function (swiper, event) {    // mobile devices don't allow audios to play automatically, it has to be triggered by a user event(click / touch).
+                        if (!$btnMusic.hasClass('paused') && bgMusic.paused) {
+                            bgMusic.play();
+                        }
+                    }
+                });
+            }
+        }, 1000);
     }
 });
 
